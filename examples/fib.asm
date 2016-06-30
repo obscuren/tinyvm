05111ffb564eca7c0ea3709846997238d8ebdf89
Jeffrey Wilcke <jeffrey@ethereum.org>
2016-06-30 21:50:25 +0200

    examples: fixed fib.asm

--- examples/fib.asm
+++ examples/fib.asm
@@ -3,24 +3,24 @@
 ; r2 = first
 ; r3 = second
 ; r4 = n
+	mov 	r4 	#5		; n = 5
+	mov 	r3 	#1		; first = 1
 
-	mov	r4 #5 	; find number 5
-	mov	r3 #1	; set r3 to 1
-for_loop:
-	lt 	r10 r0 r4
-	jmpf 	r10 end
-start_if:
-	lteq 	r10 r0 #1
-	jmpf 	r10 else
+for:
+	cmp 	r0 	r4		; if c < n
+	movgt   r15 	else		;	next = c
 
-	mov 	r1 r0
-	mov	r15 end_if
-else:
-	add 	r1 r2 r3
-	mov 	r2 r3
-	mov 	r3 r1
+	mov 	r1 	r0
+	mov 	r15 	end_if
+else					; else:
+	add 	r1 	r2 	r3	;	next = first + second
+	mov	r2 	r3		; 	first = second
+	mov 	r3 	r1		; 	second = next
 end_if:
-	add 	r0 r0 #1
-	mov 	r15 for_loop
+	add	r0 	r0 	#1
+	cmp 	r0 	r4		; for c < n; c++
+	movgt 	r15 	end
+
+	mov 	r15 	for
 end:
-	mov 	r0 r1
+	mov 	r0 	r1
